a260b39ed2e4a2f4964f449598cbbbb4d8087f5e
seungwon0113 <jungseungwon0113@gmail.com>
2025-05-17 17:37:11 +0900

    1일차

---
+++ users/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+# 사용자 모델을 정의하는 파일입니다.
+# 사용자 모델을 정의하기 위해 Django의 AbstractBaseUser와 PermissionsMixin을 상속받습니다.
+# AbstractBaseUser는 기본적인 사용자 모델을 제공하고, PermissionsMixin은 권한 관련 기능을 추가합니다.
+class UserManager(BaseUserManager):
+    def create_user(self, username, password=None):
+        if not username:
+            raise ValueError("Users must have a username")
+        user = self.model(username=username)
+        user.set_password(password)  # 비밀번호 해싱
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None):
+        user = self.create_user(username, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=128)
+    is_staff = models.BooleanField(default=False)  # 관리자인지 여부
+
+    objects = UserManager()  # 사용자 매니저 설정
+
+    USERNAME_FIELD = 'username'  # 사용자 이름 필드
+    REQUIRED_FIELDS = []  # createsuperuser 시 요구할 필드
+    def __str__(self):
+        return self.username
---
+++ users/urls.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from django.urls import path
+from users.models import User
+from django.contrib.auth import login as auth_login
+
+def index(request):
+    if request.user.is_authenticated:
+        # 사용자가 로그인한 경우
+        return render(request, "index.html", {"username": request.user.username})
+    else:
+        # 사용자가 로그인하지 않은 경우
+        return redirect("login")
+
+def login(request):
+    # 사용자가 로그인 정보를 입력하여 전송했을 때 (POST 요청)
+    if request.method == "POST":
+        username = request.POST.get("username")  # 폼에서 입력한 사용자명
+        password = request.POST.get("password")  # 폼에서 입력한 비밀번호
+
+        try:
+            user = User.objects.get(username=username)  # 사용자 검색
+            if user.check_password(password):  # 비밀번호 확인
+                auth_login(request, user)  # 로그인 처리
+                # 로그인 성공 -> index.html 렌더링
+                return redirect("index")
+            else:
+                # 비밀번호가 틀린 경우 -> 에러 메시지 포함하여 로그인 폼 다시 표시
+                return render(request, "login.html", {"error": "Invalid password"})
+        except User.DoesNotExist:
+            # 사용자가 존재하지 않는 경우 -> 에러 메시지 포함하여 로그인 폼 다시 표시
+            return render(request, "login.html", {"error": "User does not exist"})
+
+    # 이 부분이 없어서 오류가 발생했던 것! => GET 요청 처리
+    # 사용자가 로그인 페이지에 접근했을 때 (예: 링크 클릭 등)
+    return render(request, "login.html")
+
+
+
+urlpatterns = [
+    path('index/', index, name='index'),
+    path('login/', login, name='login'),
+]
