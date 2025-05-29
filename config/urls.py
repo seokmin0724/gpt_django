3a24eac68d9c1b1716cbda4c3ea6bc411ac4ad4c
seokmin0724 <parkseokmin0724@gmail.com>
2025-05-29 17:24:01 +0900

    게시판 코드를 작성 하고 실행되는지 확인

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.contrib import admin
+
+
+
+
+
+
 
 urlpatterns = [
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
-]
 
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
     path("users/", include("users.urls")),
+    path('admin/', admin.site.urls),
+    path('posts/', include('posts.urls'))
+
 ]
